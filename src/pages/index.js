dd725d3d2e48d0dbfc84060f66557da43873f98f
Duduling <duduling20@gmail.com>
2022-09-14 18:12:07 +0900

    style: 메인 링크 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,11 +15,8 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/coding-test/intro"
-          >
-            Go to Blog
+          <Link className="button button--secondary button--lg" to="/post/2022">
+            {`Go to Logs in ${new Date().getFullYear()}`}
           </Link>
         </div>
       </div>
