c8f4c1c1b86de1909ed1bdaad816bd25dd5681a7
duduling <duduling20@gmail.com>
2026-07-09 17:05:50 +0900

    style: 코드트리 제거

--- docs/01_coding-test/codetree/trail2/Time to Time/time-to-time.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split('\n');
-const [a, b, c, d] = input[0].split(' ').map(Number);
-
-console.log((60 * c + d) - (60 * a + b))
